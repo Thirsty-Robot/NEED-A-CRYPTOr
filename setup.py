23978f0d5fd7f44b5e98c6434c361439f6f70844
Thirsty-Robot <Thirsty-Robot@protonmail.com>
2018-04-21 11:37:56 -0500

    Added __init__.py and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="NEED-A-CRYPTOR",
+	version="1.0.0",
+	description="A ROT-13 crypter and decrypter",
+	author="Thirsty-Robot",
+	author_email="Thirsty-Robot@protonmail.com",
+	packages=["src"]
+)
